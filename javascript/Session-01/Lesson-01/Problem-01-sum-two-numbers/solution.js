24d6e4ac86ce7404619d927b7e0173e9a9aec27e
warmdev17 <165092497+warmdev17@users.noreply.github.com>
2026-05-20 16:58:46 +0000

    Solve sum-two-numbers

--- javascript/Session-01/Lesson-01/Problem-01-sum-two-numbers/solution.js
+++ javascript/Session-01/Lesson-01/Problem-01-sum-two-numbers/solution.js
@@ -1,10 +1,3 @@
 const fs = require('fs');
-
-
-// djksakldjkasldjaskl
-// hihi
-// xin chào
-// dkjsakdjaslkdjsakdjsakjdsla
-// aksdjkladjsalkdjskaldj
 const input = fs.readFileSync(0, 'utf8').trim().split(' ').map(Number);
 console.log(input[0] + input[1]);
